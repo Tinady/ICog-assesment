3b7da47fab408effe851dc59a7d399173adc35ea
Tina Dessu <tinady2009@gmail.com>
2023-07-04 11:44:13 +0300

    Update App.jsx

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import Header from './components/header'
+import Main from './components/main'
 import './App.css'
 
 function App() {
@@ -6,7 +8,8 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Header/>
+       <Main/>
     </div>
   )
 }
