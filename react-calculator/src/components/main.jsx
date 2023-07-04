f46077178ec16c8b86758df55a07addcbf988ee2
Tina Dessu <tinady2009@gmail.com>
2023-07-04 19:38:45 +0300

    Update main.jsx

--- react-calculator/src/components/main.jsx
+++ react-calculator/src/components/main.jsx
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useState } from "react";
+import DigitButton from "./DigitButton";
+
+
+ export const ACTIONS={
+
+  CLEAR:'clear',
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CHOOSE_SIGN: 'choose_sign',
+  EQUAL: 'equal'
+
+
+
+
+ }
+
+
+
+ function reducer(state, {type, payload}){
+
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      
+      if (payload.digit==='0'&& state.currentOperand ==='0') return state
+      if (payload.digit==='.'&& state.currentOperand.includes('.')) return state
+      return{
+      ...state,
+      currentOperand: `${state.currentOperand|| ''}${payload.digit}`
+   }
+
+   case ACTIONS.CLEAR:
+     return {
+      ...state,
+      currentOperand:''
+    }
+
+
+     
+
+
+  }
+
+ }
+
 
 export default function main(){
-  const [currentValue,  setCurrentValue]=useState('0')
+   
+  const [state, dispatch]=useReducer(reducer,{currentOperand:'', previousOperand:'', operation:''})
 
-
+  
 
   return(
    <main>
      <div className="calc-container">
       <div> 
-         <div className="cview">{currentValue}</div>
+         <div className="cview">
+          <p>{state.previousOperand}</p>
+          <p>{state.currentOperand}</p>
+          </div>
+         
          <p className="button-tooltip"> Cview</p>
 
       </div>
 
       <div className="buttons">
        <div className="button-container">
-         <button className="button-name"> Ac</button>
+         <button className="button-name" onClick={()=> dispatch({type:ACTIONS.CLEAR})}> Ac</button>
          <p className="button-tooltip"> Ac</p>
        </div>
 
@@ -38,18 +87,18 @@ export default function main(){
 
 
        <div className="button-container">
-         <button className="button-name"> 7</button>
+         <DigitButton digit='7' dispatch={dispatch}/>
          <p className="button-tooltip"> 7</p>
        </div>
 
 
        <div className="button-container">
-         <button className="button-name"> 8</button>
+       <DigitButton digit='8' dispatch={dispatch}/>
          <p className="button-tooltip"> 8</p>
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 9</button>
+       <DigitButton digit='9' dispatch={dispatch}/>
          <p className="button-tooltip"> 9</p>
        </div>
 
@@ -59,18 +108,18 @@ export default function main(){
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 4</button>
+       <DigitButton digit='4' dispatch={dispatch}/>
          <p className="button-tooltip"> 4</p>
        </div>
 
 
        <div className="button-container">
-         <button className="button-name"> 5</button>
+       <DigitButton digit='5' dispatch={dispatch}/>
          <p className="button-tooltip"> 5</p>
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 6</button>
+       <DigitButton digit='6' dispatch={dispatch}/>
          <p className="button-tooltip"> 6</p>
        </div>
 
@@ -80,17 +129,17 @@ export default function main(){
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 1</button>
+       <DigitButton digit='1' dispatch={dispatch}/>
          <p className="button-tooltip"> 1</p>
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 2</button>
+       <DigitButton digit='2' dispatch={dispatch}/>
          <p className="button-tooltip"> 2</p>
        </div>
 
        <div className="button-container">
-         <button className="button-name"> 3</button>
+       <DigitButton digit='3' dispatch={dispatch}/>
          <p className="button-tooltip"> 3</p>
        </div>
 
@@ -102,12 +151,12 @@ export default function main(){
        <div className="last-row">
 
        <div className="button-container">
-         <button className="button-name"> 0</button>
+       <DigitButton digit='0' dispatch={dispatch}/>
          <p className="button-tooltip"> 0</p>
        </div>
 
        <div className="button-container">
-         <button className="button-name"> .</button>
+       <DigitButton digit='.' dispatch={dispatch}/>
          <p className="button-tooltip"> .</p>
        </div>
 
