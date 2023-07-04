ae844ecf3ac7b391043d85a18a04d0a58d371b3e
Tina Dessu <tinady2009@gmail.com>
2023-07-04 11:44:32 +0300

    Create header.jsx

---
+++ react-calculator/src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import icon from  '../assets/icon.svg'
+
+
+
+ export default  function header(props){
+    return(
+        <nav className={props.darkMode?'dark':''}>
+            
+                <div className="icon-container">
+               <img src={icon} className="calc-icon"/>
+               <h3 className="calc-title">Reactive calculator</h3>
+               </div>
+               <div className="toggle-Container" >
+          <div  className="toggle-slider"  onClick={props.handleClick}>
+           <div className="toggle-circle"></div>
+           </div>
+            </div>
+        
+
+        </nav>
+    )
+ }
