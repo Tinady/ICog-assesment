a2a8157a03df64e69c50727608fc725fb503c3eb
Tina Dessu <tinady2009@gmail.com>
2023-07-05 17:43:23 +0300

    Update header.jsx

--- react-calculator/src/components/header.jsx
+++ react-calculator/src/components/header.jsx
@@ -6,7 +6,7 @@ import icon from  '../assets/icon.svg'
  export default  function header(props){
     return(
         <nav className={props.darkmode?'nav-dark':''}>
-            
+            <div className="header-container">
                 <div className="icon-container">
                <img src={icon} className="calc-icon"/>
                <h3 className="calc-title">Reactive calculator</h3>
@@ -15,6 +15,8 @@ import icon from  '../assets/icon.svg'
           <div  className="toggle-slider"  onClick={props.handleClick}>
            <div className="toggle-circle"></div>
            </div>
+            </div>
+
             </div>
         
 
